e621f1d977cf5877950279e4a16a97fe4732808e
unknown <vmatthew360@gmail.com>
2021-10-03 20:54:35 -0500

    added option change functions

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,6 @@ function Init() {
 
 Init();
 
-// d3.selectAll("#selDataset").on("change", demoTable(demographics);
 
 
 function demoTable(item) {
@@ -30,9 +29,33 @@ function demoTable(item) {
     var dataid = MetaData.filter(data => data.id == item);
     var htmldata = d3.select("#sample-metadata").html("");
 
+    console.log("Metadata:")
+    console.log(MetaData)
+
     Object.entries(dataid[0]).forEach(([key, value]) => {
         htmldata.append("p").text(`${key}: ${value}`);
      });   
   });
 }
 
+
+//Creating the function to hold the charts
+function charts(item) {
+
+  d3.json("data/data.json").then((sampleData) => {
+
+    var filtereddata = sampleData.samples;
+    console.log("filetered data:")
+    console.log(filtereddata)
+
+    // filter the data for charting
+    var dictSample = filtereddata.filter(data => data.id == item)[0];
+
+
+
+//Define the change option
+function optionChanged(itemOption)  {
+  //Update table with changed id
+  demoTable(itemOption);
+
+}
